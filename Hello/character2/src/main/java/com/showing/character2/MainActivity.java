7bfa13d1b16b7967b014dd05a4200d38ee09dbd5
unknown <943477687@qq.com>
2019-07-12 21:46:51 +0800

    Upadte the chapter1-Hello add the 2en module named character2

--- Hello/character2/src/main/java/com/showing/character2/MainActivity.java
+++ Hello/character2/src/main/java/com/showing/character2/MainActivity.java
@@ -3,6 +3,12 @@ package com.showing.character2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.showing.character2.R.id.tv_tv1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv=findViewById(tv_tv1);
+        //tv.setEllipsize();
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Log.d(TAG, "onClick: ");
+                //Toast.makeText()
+            }
+        });
     }
 }
--- Hello/character2/src/main/java/com/showing/character2/SecondActivity.java
+++ Hello/character2/src/main/java/com/showing/character2/SecondActivity.java
@@ -1,4 +1,44 @@
 package com.showing.character2;
 
-public class SecondActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+
+public class SecondActivity extends Activity {
+    private static final String TAG="SecondActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("On-creat");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("On-destory");
+    }
 }
+
